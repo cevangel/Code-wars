f91d6df33400f77c782279e7f79ba701854aff0b
cevangel <cevangdpt@gmail.com>
2024-07-15 02:02:54 -0400

    Add coding challenge

---
+++ 8kyu/grasshopper-check-for-factor/main.js
@@ -0,0 +1,6 @@
+function checkForFactor (base, factor) {
+  // code here
+
+ return base % factor === 0 ? true : false; //ternary operator needs return in front
+  //aka return (condition) ? true: false;
+}
