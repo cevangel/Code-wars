c95bedfad8425256134c2a52e3df4291a769670e
cevangel <cevangdpt@gmail.com>
2024-08-30 19:31:49 -0400

    Add coding challenge for 8-30-24

---
+++ 8kyu/hello-world/main.js
@@ -0,0 +1,23 @@
+// //**prompt
+// Description:
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+// Fundamentals
+
+
+//**My solution 
+function greet() {
+  return "hello world!";
+  }
+
+//**tests it passed
+//see entry
+
+
+
+
+//**other solutions to learn from 
+const greet = () => "hello world!";
