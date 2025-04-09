9f17b3ed3033da3ce6b84bc1f36d93fde874158e
Christopher Evangelista <cevangdpt@gmail.com>
2025-04-09 10:59:23 -0400

    Add coding challenge

--- 8kyu/is-n-divisible-by-x-and-y/main.js
+++ 8kyu/is-n-divisible-by-x-and-y/main.js
@@ -1,3 +1,4 @@
+// 
 // https://www.codewars.com/kata/5545f109004975ea66000086/solutions/javascript?filter=all&sort=best_practice&invalids=false
 function isDivisible(n, x, y) {
   return n % x === 0 && n % y === 0
