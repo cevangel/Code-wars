cd4adb7d44422878ed8faeb6c119ab7767145430
Christopher Evangelista <cevangdpt@gmail.com>
2025-04-11 11:00:42 -0400

    Add coding challenge

---
+++ 8kyu/jennys-secret-message/main.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/55225023e1be1ec8bc000390/javascript
+function greet(name){
+  return name === "Johnny" ? "Hello, my love!" : `Hello, ${name}!`;
+}
+
+// other solutions: https://www.codewars.com/kata/55225023e1be1ec8bc000390/solutions/javascript?filter=all&sort=best_practice&invalids=false
