03d3cf723b77d4537d4df8f25c58fd4646e17a9c
Christopher Evangelista <cevangdpt@gmail.com>
2025-04-23 11:01:51 -0400

    Add coding challenge

---
+++ 8kyu/what-is-between/main.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/55ecd718f46fba02e5000029/solutions/javascript?filter=me&sort=best_practice&invalids=false
+function between(a, b) {
+  let array = [];
+  for(let i = a; i <= b; i++){
+    array.push(i);
+  }
+  return array;
+}
