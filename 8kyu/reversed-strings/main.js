b156201712c8f41a235bf813beb06bbcf4d36b3b
Christopher Evangelista <cevangdpt@gmail.com>
2025-05-21 10:56:12 -0400

    Add coding challenge

--- 8kyu/reversed-strings/main.js
+++ 8kyu/reversed-strings/main.js
@@ -1,3 +1,10 @@
-function solution(str){
-  return str.split('').reverse().join('');  
+function solution(str){  //input a string
+  let result = [...str].reduce((acc, letter)=> letter + acc, ""); //remember split parameters(""), and acc is first parameter in reduce
+  return result //spread can be used in place of str.split('')
+  //output reversed string (blank strings are blank, single letters are single )
 }
+
+// // Old solution here
+// function solution(str){
+//   return str.split('').reverse().join('');  
+// }
