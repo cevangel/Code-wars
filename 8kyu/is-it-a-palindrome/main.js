61abaee96dea06a3cbea36c4a07d366d3292d19c
Christopher Evangelista <cevangdpt@gmail.com>
2025-05-28 01:20:37 -0400

    Add coding challenge

--- 8kyu/is-it-a-palindrome/main.js
+++ 8kyu/is-it-a-palindrome/main.js
@@ -1,3 +1,10 @@
-function testEven(n) {
-  return n%2 === 0 ? true: false;   //Your awesome code here!
+// from here: https://web.archive.org/web/20210226224607/https://scotch.io/courses/the-ultimate-guide-to-javascript-algorithms/palindromes
+function isPalindrome(x) {
+  if (x === ""){return true}
+  let string = x.toLowerCase();
+  for (let i=0; i < string.length/2; i++){
+    return string[0] !== string[string.length-1] ? false : true;
   }
+//   return x.toLowerCase().split('').reduce((acc, current)=> current + acc, "") === x.toLowerCase()
+  // your code here
+}
