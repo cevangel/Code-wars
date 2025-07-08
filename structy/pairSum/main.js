c7f0c0a0606e6940fd477b11b76de83713bc6a53
Christopher Evangelista <cevangdpt@gmail.com>
2025-07-08 00:38:03 -0400

    Update coding challenge

--- structy/pairSum/main.js
+++ structy/pairSum/main.js
@@ -14,4 +14,19 @@
 //   pairSum,
 // };
 
+const pairSum = (numbers, targetSum) => { //time/space: O(n)
+  let previous = {}; // lookup data structure of nums seen and their index
+  for (let i=0; i<=numbers.length; i++){
+    const num = numbers[i]; 
+    const complement = targetSum - num;
+    //if complement is in previous, then return  i, previous[complement]
+    if (complement in previous) { 
+      return [i, previous[complement]];//both give index 
+  }
+    previous[num] = i; //after if, at end of each loop, add num to prev
+  }
+};
 
+module.exports = {
+  pairSum,
+};
